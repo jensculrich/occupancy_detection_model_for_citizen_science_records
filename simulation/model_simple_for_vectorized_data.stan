ec4a8920a5f20032010e4e1413d251915f98de6d
jensculrich <jensj27@gmail.com>
2022-10-10 17:21:47 -0700

    created vectorized simulation data and updating model to handle vectorized simulation data rather than arrayed data

---
+++ simulation/model_simple_for_vectorized_data.stan
@@ -0,0 +1,177 @@
+// multi-species occupancy model for GBIF occurrence data
+
+functions{
+  
+  // if the species is detected at the site*interval at least once..
+  real lp_observed(int V, real logit_psi, real logit_p){ 
+    
+    return log_inv_logit(logit_psi) + 
+            // probability density of getting an occurrence 
+            // of a species at a site*interval plus..
+            binomial_logit_lpmf(V | 1, logit_p); 
+            // probability density of then observing or not observing  
+            // that specific species at the site*interval per single visit l
+  }
+  
+  // if the species is never detected at the site*interval..
+  real lp_unobserved(real logit_psi, real logit_p){ 
+
+    return log_sum_exp(log_inv_logit(logit_psi) +
+           // probability density of the species occupying the site*interval
+           // but not being detected at the visit l plus..
+           log1m_inv_logit(logit_p),
+           // probability density of not detecting it on the visit l
+           
+           // summed with..
+           log1m_inv_logit(logit_psi));
+           // probability density of the species NOT occupying the site*interval
+           // and therefore detection is not possible
+  }
+  
+}
+
+data {
+  
+  int<lower=1> R; // number of interval*site*species*visit combinations
+  
+  int<lower=1> n_species;  // observed species
+  int<lower=1> species[R];      // vector of species
+  
+  int<lower=1> n_sites;  // sites within region
+  int<lower=1> site[R];      // vector of sites
+  
+  int<lower=1> n_intervals;  // intervals during which sites are visited
+  int<lower=1> interval[R]; // vector of intervals (used as covariate data for 
+                                // fixed effect of occupancy interval (time) on occupancy)
+  
+  int<lower=1> n_visits; // visits within intervals
+  
+  int<lower=0> V[R, n_visits];  // visits when species i was detected at site j on interval k
+  
+} // end data
+
+
+parameters {
+  
+  // OCCUPANCY
+  real mu_psi_0; // global interecept for occupancy
+  
+  // species specific intercept allows some species to occur at higher rates than others, 
+  // but with overall estimates for occupancy partially informed by the data pooled across all species.
+  vector[n_species] psi_sp; // species specific intercept for occupancy
+  real<lower=0> sigma_psi_species; // variance in species intercepts
+  
+  // random slope for species specific temporal effects on occupancy
+  vector[n_species] psi_interval; // vector of species specific slope estimates
+  real mu_psi_interval; // community mean of species specific slopes
+  real<lower=0> sigma_psi_interval; // variance in species slopes
+  
+  // DETECTION
+  real mu_p_0;
+  
+  // species specific intercept allows some species to be detected at higher rates than others, 
+  // but with overall estimates for occupancy partially informed by the data pooled across all species.
+  vector[n_species] p_sp; // species specific intercept for detection
+  real<lower=0> sigma_p_species; // variance in species intercepts
+  
+  // random slope for site specific temporal effects on occupancy
+  vector[n_sites] p_site; // vector of spatially specific slope estimates
+  real<lower=0> sigma_p_site; // variance in site slopes
+  
+  real p_interval; // fixed temporal effect on detection probability
+  
+} // end parameters
+
+
+transformed parameters {
+  
+  real logit_psi[n_species*n_sites*n_intervals];  // log odds  of occurrence
+  real logit_p[R, n_visits];  // log odds of detection
+  
+  for (i in 1:R){   // loop across all species*site*intervals
+          
+          logit_psi[i] = // log odds  of occurrence is equal to
+            mu_psi_0 + // a baseline intercept
+            psi_sp[species[i]] + // a species specific intercept
+            psi_interval[species[i]]*interval[i]; // a species specific temporal effect
+            
+  }
+  
+  for (i in 1:R){   // loop across all species*site*intervals
+    for (j in 1:n_visits){
+          
+          logit_psi[i, j] = // log odds  of occurrence is equal to
+            mu_p_0 + // a baseline intercept
+            p_sp[species[i]] + // a species specific intercept
+            p_site[sites[j]] + // a spatially specific intercept
+            p_interval*intervals[k]; // an overall effect of time on detection
+            
+    }       
+  }
+  
+} // end transformed parameters
+
+
+model {
+  // PRIORS
+  
+  // Occupancy (Ecological Process)
+  mu_psi_0 ~ cauchy(0, 2.5); // global intercept for occupancy rate
+  
+  psi_sp ~ normal(0, sigma_psi_species); 
+  // occupancy intercept for each species drawn from the community
+  // distribution (variance defined by sigma), centered at 0. 
+  sigma_psi_species ~ cauchy(0, 2.5);
+  
+  psi_interval ~ normal(0, sigma_psi_interval);
+  // occupancy slope (temporal effect on occupancy) for each species drawn from the 
+  // community distribution (variance defined by sigma), centered at mu_psi_interval. 
+  // centering on mu (rather than 0) allows us to estimate the average effect of
+  // the management on abundance across all species.
+  sigma_psi_interval ~ cauchy(0, 2.5); // community variance
+  
+  // Detection (Observation Process)
+  mu_p_0 ~ cauchy(0, 2.5); // global intercept for detection
+  
+  p_sp ~ normal(0, sigma_p_species); 
+  // detection intercept for each species drawn from the community
+  // distribution (variance defined by sigma), centered at 0. 
+  sigma_p_species ~ cauchy(0, 2.5);
+  
+  // multivariate hierarchical prior
+  p_site ~ normal(0, sigma_p_site);
+  // detection intercept for each site*interval drawn from the spatiotemporal
+  // distribution (variance defined by sigma), centered at 0. 
+  sigma_p_site ~ cauchy(0, 2.5); // spatiotemporal variance
+  
+  p_interval ~ cauchy(0, 2.5);
+  
+  // LIKELIHOOD
+  // Stan can sample mean and sd of parameters by summing out the
+  // parameter (marginalizing) across likelihood statements
+  for(i in 1:nsp) { // loop across all species
+    for(j in 1:nsite) { // loop across all sites
+      for(k in 1:ninterval){
+        for(l in 1:nvisit){
+          
+          // if species is detected at the specific site*interval at least once
+          // lp_observed calculates the probability density that occurs given logit_psi plus
+          // the probability density that we did/did not observe it on each visit l in 1:nvisit
+          if(sum(V[i, j, k, 1:nvisit]) > 0){ 
+            target += lp_observed(V[i, j, k, l], 
+              logit_psi[i, j, k], logit_p[i, j, k, l]);
+          
+          // else the species was never detected at the site*interval
+          // lp_unobserved sums the probability density of:
+          // 1) species occupies the site*interval but was not detected on each visit, and
+          // 2) the species does not occupy the site*interval
+          } else {
+            target += lp_unobserved(logit_psi[i, j, k], logit_p[i, j, k, l]);
+            
+          } // end if/else
+          
+        }
+      }
+    }
+  }
+} // end model
--- simulation/simulate_simple.R
+++ simulation/simulate_simple.R
@@ -4,6 +4,263 @@
 # for any species were probably not visited or that sites with only detections for
 # a few species only might be targeted and not fully community wide)
 
+
+## --------------------------------------------------
+### Let's try to reform the data into a series of vectors rather than a big array
+# I'm having a more difficult time keeping track of all of the random effects in the array form
+# and this format has worked for me before. Are there any drawbacks to having the data in a series of
+# vectors? Is it more computationally intensive?
+library(tidyverse)
+
+## --------------------------------------------------
+### Variable values for data simulation
+
+## study dimensions
+n_species=10 ## number of species (number of species included in the studies taxonomic scope)
+n_sites=10 ## number of sites (number of cities included in the study)
+n_intervals=10 ## number of occupancy intervals (i.e. number of sets of years within which we are estimating occupancy rates)
+n_visits=3 ## number of repeat 'surveys' per interval (i.e. number of years within an interval in which data is collected)
+
+# number of unique data observation points 
+# (observations of unique species*site*occupancy_interval*visit combinations)
+R = n_species*n_sites*n_intervals
+n_data_per_species <- R/n_species
+n_data_per_site <- R/n_sites
+n_data_per_interval <- R/n_intervals
+
+## occupancy
+mu_psi_0 = 0
+sigma_psi_sp = 0.5
+mu_psi_interval = 0.5
+sigma_psi_interval = 0.2
+
+## detection
+mu_p_0 = 0
+sigma_p_species = 0.3
+sigma_p_site = 0.3
+p_interval = 0.5
+
+## expit and logit functions
+expit <- function(x) 1/(1+exp(-x))
+logit <- function(x) log(x/(1-x))
+
+## --------------------------------------------------
+### Covariates
+
+interval <- rep(1:n_intervals, each = n_data_per_interval)
+
+site <- rep(1:n_sites, each = n_species, 
+             times = n_data_per_site/n_sites)
+
+species <- rep(1:n_species, times = 
+                 n_data_per_species)
+
+visit <- rep(1:n_visits, R/n_visits)
+
+View(as.data.frame(cbind(interval, site, species)))
+
+## --------------------------------------------------
+### Ecological process
+
+## add a species-level random effect for occupancy intercept
+# (some species more frequently than others), centered on 0
+species_intercepts_psi <- rnorm(n_species, 0, sigma_psi_sp)
+(mean(species_intercepts_psi)) # should be near 0 since we are centered on 0
+(sd(species_intercepts_psi)) # should be near value of sigma_psi_sp
+
+psi_species <- as.numeric(vector(length=R))
+psi_species <- rep(species_intercepts_psi[1:n_species], times = n_data_per_species)
+
+# and a species-level random slope (change in occupancy through time
+# allowed to respond uniquely by species)
+# centered on community mean and defined variation
+species_slopes_psi <- rnorm(n_species, mu_psi_interval, sigma_psi_interval)
+(mean(species_slopes_psi)) # should be near mu_alpha1
+
+psi_occupancy_interval <- as.numeric(vector(length=R))
+psi_occupancy_interval <- rep(species_slopes_psi[1:n_species], times = n_data_per_species)
+
+test <- cbind(interval, site, species, psi_species, psi_occupancy_interval)
+
+### now generate occupancy probabilities
+psi_vector = as.numeric(vector(length=R))
+
+for(i in 1:R) { # for each 
+      
+  psi_vector[i] <- expit( # occupancy is equal to
+          mu_psi_0 + # a baseline intercept
+          psi_species[i] + # a species specific intercept
+          psi_occupancy_interval[i]*(interval[i])) # a species specific temporal change
+      
+}
+
+psi_data <- as.data.frame(cbind(
+  interval, site, species, psi_species, psi_occupancy_interval, psi_vector))
+
+## --------------------------------------------------
+## Generate presence and absence data
+
+# Z[i]is the actual outcome of the occupancy coin flip based on weight captured in psi_vector[i]
+Z = as.numeric(vector(length=R))
+
+for(i in 1:R) { # for each 
+  
+  # eventually here we will need to specify which(site) to restrict to actual range
+  Z[i] <- rbinom(n = 1, size = 1, prob = psi_vector[i])
+  
+}
+
+psi_data_simmed <- as.data.frame(cbind(
+  interval, site, species, psi_species, psi_occupancy_interval, psi_vector, Z))
+
+## --------------------------------------------------
+### Observation process
+
+## add a species-level random effect for occupancy intercept
+# (some species more frequently observed than others), centered on 0
+species_intercepts_p <- rnorm(n_species, 0, sigma_p_species)
+(mean(species_intercepts_p)) # should be near 0 since we are centered on 0
+(sd(species_intercepts_p)) # should be near value of sigma_psi_sp
+
+p_species <- as.numeric(vector(length=R))
+p_species <- rep(species_intercepts_p[1:n_species], times = n_data_per_species)
+
+## add a site-level random effect for occupancy intercept
+# (some sites more effectively surveyed/searched than others), centered on 0
+site_intercepts_p <- rnorm(n_sites, 0, sigma_p_site)
+(mean(site_intercepts_p)) # should be near 0 since we are centered on 0
+(sd(site_intercepts_p)) # should be near value of sigma_psi_sp
+
+p_site <- as.numeric(vector(length=R))
+p_site <- rep(site_intercepts_p[1:n_sites], times = n_data_per_site)
+
+test3 <- cbind(interval, site, species, p_species, p_site)
+
+### now generate detection probabilities
+p_vector = as.numeric(vector(length=R))
+
+for(i in 1:R) { # for each visit
+  
+  p_vector[i] <- expit( # detection is equal to 
+      mu_p_0 + # a baseline intercept
+      p_species[i] + # a species specific intercept
+      p_site[i] + # a spatially specific intercept
+      p_interval*(interval[i])) # an overall effect of time on detection
+  
+} # for each visit
+
+test4 <- cbind(interval, site, species, p_species, p_site, p_vector)
+
+## --------------------------------------------------
+## Generate some presence/absence detection data
+
+V = matrix(NA, nrow = length(p_vector), ncol = n_visits) # Array for counts
+
+for(i in 1:R) { # for each 
+  for(j in 1:n_visits){
+  
+  # eventually here we will need to specify which(site) to restrict to actual range
+  V[i, j] <- Z[i] * # occupancy state * detection prob
+    rbinom(n = 1, size = 1, prob = p_vector[i])
+  
+  }
+}
+
+View(as.data.frame(V))
+
+# species should be detected some of the time when they are there, but never when Z = 0.
+# View(cbind(Z, V))
+
+# should capture all above into a function
+
+## --------------------------------------------------
+### Prepare data for model
+
+# data to feed to the model
+R <- R # number of species*site*survey combinations
+V <- V # detection data
+n_species <- n_species # number of species
+n_sites <- n_sites # number of sites
+n_intervals <- n_intervals # number of surveys 
+n_visits <- n_visits
+
+interval <- interval
+site <- site
+species <- species
+visit <- visit
+
+stan_data <- c("R", "V", "n_intervals", "n_sites", "n_species", "n_visits", 
+               "species", "site", "interval", "visit")
+
+# Parameters monitored
+params <- c("mu_psi_0",
+            "psi_sp",
+            "sigma_psi_species",
+            "psi_interval",
+            "mu_psi_interval",
+            "sigma_psi_interval",
+            "mu_p_0",
+            "p_sp",
+            "sigma_p_species",
+            "p_site",
+            "sigma_p_site",
+            "p_interval"
+)
+
+
+# MCMC settings
+n_iterations <- 400
+n_thin <- 1
+n_burnin <- 200
+n_chains <- 3
+n_cores <- 3
+
+## Initial values
+# given the number of parameters, the chains need some decent initial values
+# otherwise sometimes they have a hard time starting to sample
+inits <- lapply(1:n_chains, function(i)
+  list(mu_psi_0 = runif(1, -1, 1),
+       psi_sp = runif(1, -1, 1),
+       sigma_psi_species = runif(1, -1, 1),
+       psi_interval = runif(1, -1, 1),
+       mu_psi_interval = runif(1, -1, 1),
+       sigma_psi_interval = runif(1, -1, 1),
+       mu_p_0 = runif(1, -1, 1),
+       p_sp = runif(1, -1, 1),
+       sigma_p_species = runif(1, -1, 1),
+       p_site = runif(1, -1, 1),
+       sigma_p_site = runif(1, -1, 1),
+       p_interval = runif(1, -1, 1)
+       
+  )
+)
+
+#targets <- as.data.frame(cbind(param_names, parameters))
+
+## --------------------------------------------------
+### Run model
+library(rstan)
+stan_model <- "./simulation/model_simple.stan"
+
+## Call Stan from R
+stan_out_sim <- stan(stan_model,
+                   data = stan_data, 
+                   init = inits, 
+                   pars = params,
+                   chains = n_chains, iter = n_iterations, 
+                   warmup = n_burnin, thin = n_thin,
+                   seed = 1,
+                   open_progress = FALSE,
+                   cores = n_cores)
+
+print(stan_out_sim, digits = 3)
+
+saveRDS(stan_out_sim, ".//stan_out_sim.rds")
+stan_out_sim <- readRDS(".//stan_out_sim.rds")
+
+## --------------------------------------------------
+### In Shirey et al. 2022 they use the data in an array format simulated as below ->
+
 ## --------------------------------------------------
 ### Variable values for data simulation
 ## study dimensions
@@ -82,17 +339,17 @@ for(site in 1:nsite) { # for each site
     for(sp in 1:nsp) { # for each species
       
       psi.mat[sp,site,interval] <- expit( # occupancy is equal to
-                                     mu.psi.0 + # a baseline intercept
-                                     psi.sp[sp] + # a species specific intercept
-                                     psi.interval[sp]*(interval)) # a species specific temporal change
+        mu.psi.0 + # a baseline intercept
+          psi.sp[sp] + # a species specific intercept
+          psi.interval[sp]*(interval)) # a species specific temporal change
       
       for(visit in 1:nvisit) { # for each visit
         
         p.mat[sp,site,interval,visit] <- expit( # detection is equal to 
-                                           mu.p.0 + # a baseline intercept
-                                           p.sp[sp] + # a species specific intercept
-                                           p.site[site] + # a spatiotemporally specific intercept
-                                           p.interval*(interval)) # an overall effect of time on detection
+          mu.p.0 + # a baseline intercept
+            p.sp[sp] + # a species specific intercept
+            p.site[site] + # a spatiotemporally specific intercept
+            p.interval*(interval)) # an overall effect of time on detection
       } # for each visit
     } # for each species
   } # for each interval
@@ -116,10 +373,10 @@ Z <- array(NA, dim=c(nsp=nsp,
 for(interval in 1:ninterval){
   for(site in 1:nsite){
     for(sp in 1:nsp){
-    
-    # eventually here we will need to specify which(site) to restrict to actual range
-    Z[sp,site,interval] <- rbinom(n = 1, size = 1, prob = psi.mat[sp,site,interval])
-    
+      
+      # eventually here we will need to specify which(site) to restrict to actual range
+      Z[sp,site,interval] <- rbinom(n = 1, size = 1, prob = psi.mat[sp,site,interval])
+      
     }
   }
 }
@@ -139,8 +396,8 @@ for(sp in 1:nsp){
         
         # eventually here we will need to specify which(site) to restrict to actual range
         V[sp,site,interval,v] <- Z[sp,site,interval] * # occupancy state * detection prob
-                              rbinom(n = 1, size = 1, prob = p.mat[sp,site,interval,v])
-    
+          rbinom(n = 1, size = 1, prob = p.mat[sp,site,interval,v])
+        
       }
     }
   }
@@ -162,69 +419,3 @@ species <- seq(1, nsp, by=1)
 
 stan_data <- c("V", "nsp", "nsite", "ninterval", "nvisit", 
                "species", "sites", "intervals")
-
-# Parameters monitored
-params <- c("mu_psi_0",
-            "psi_sp",
-            "sigma_psi_species",
-            "psi_interval",
-            "mu_psi_interval",
-            "sigma_psi_interval",
-            "mu_p_0",
-            "p_sp",
-            "sigma_p_species",
-            "p_site",
-            "sigma_p_site",
-            "p_interval"
-)
-
-
-# MCMC settings
-n_iterations <- 400
-n_thin <- 1
-n_burnin <- 200
-n_chains <- 3
-n_cores <- 3
-
-## Initial values
-# given the number of parameters, the chains need some decent initial values
-# otherwise sometimes they have a hard time starting to sample
-inits <- lapply(1:n_chains, function(i)
-  list(mu_psi_0 = runif(1, -1, 1),
-       psi_sp = runif(1, -1, 1),
-       sigma_psi_species = runif(1, -1, 1),
-       psi_interval = runif(1, -1, 1),
-       mu_psi_interval = runif(1, -1, 1),
-       sigma_psi_interval = runif(1, -1, 1),
-       mu_p_0 = runif(1, -1, 1),
-       p_sp = runif(1, -1, 1),
-       sigma_p_species = runif(1, -1, 1),
-       p_site = runif(1, -1, 1),
-       sigma_p_site = runif(1, -1, 1),
-       p_interval = runif(1, -1, 1)
-       
-  )
-)
-
-#targets <- as.data.frame(cbind(param_names, parameters))
-
-## --------------------------------------------------
-### Run model
-library(rstan)
-stan_model <- "./simulation/model_simple.stan"
-
-## Call Stan from R
-stan_out_sim <- stan(stan_model,
-                   data = stan_data, 
-                   init = inits, 
-                   pars = params,
-                   chains = n_chains, iter = n_iterations, 
-                   warmup = n_burnin, thin = n_thin,
-                   seed = 1,
-                   open_progress = FALSE,
-                   cores = n_cores)
-
-print(stan_out_sim, digits = 3)
-
-saveRDS(stan_out_sim, ".//stan_out_sim.rds")
-stan_out_sim <- readRDS(".//stan_out_sim.rds")
