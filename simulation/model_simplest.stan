f425ec1fcf9e0271bf45f2d3014fcde251e4f524
jensculrich <jensj27@gmail.com>
2022-10-11 16:35:17 -0700

    removed simple model and created "model_simplest.stan". With model_simplest I rewrote the model from the ground up to identify and eliminate any problems while the model was building up in complexity. model_simplest.stan now accurately and reliabely returns parameter values used to simulate observation data in "simulate_simplest.R"

--- simulation/model_simplest.stan
+++ simulation/model_simplest.stan
@@ -1,54 +1,26 @@
 // multi-species occupancy model for GBIF occurrence data
-
-functions{
-  
-  // if the species is detected at the site*interval at least once..
-  real lp_observed(int V, real logit_psi, real logit_p){ 
-    
-    return log_inv_logit(logit_psi) + 
-            // probability density of getting an occurrence 
-            // of a species at a site*interval plus..
-            binomial_logit_lpmf(V | 1, logit_p); 
-            // probability density of then observing or not observing  
-            // that specific species at the site*interval per single visit l
-            
-  } // end lp_observed
-  
-  // if the species is never detected at the site*interval..
-  real lp_unobserved(real logit_psi, real logit_p){ 
-
-    return log_sum_exp(log_inv_logit(logit_psi) +
-           // probability density of the species occupying the site*interval
-           // but not being detected at the visit l plus..
-           log1m_inv_logit(logit_p),
-           // probability density of not detecting it on the visit l
-           
-           // summed with..
-           log1m_inv_logit(logit_psi));
-           // probability density of the species NOT occupying the site*interval
-           // and therefore detection is not possible
-           
-  } // end lp_unobserved
-  
-} // end functions
+// jcu, started oct 11, 2022.
+// should be expanded to 1) accomodate species whose range is restricted to some portion of 
+// the broader spatial extent and 2) consider that perhaps not all species are sampled 
+// by data collectors in a given 'visit' to a site (or that no species are sampled).
 
 data {
   
-  int<lower=1> R; // number of interval*site*species*visit combinations
-  
   int<lower=1> n_species;  // observed species
-  int<lower=1> species[R];      // vector of species
+  int<lower=1> species[n_species];      // vector of species
   
   int<lower=1> n_sites;  // sites within region
-  int<lower=1> site[R];      // vector of sites
+  int<lower=1> sites[n_sites];      // vector of sites
   
   int<lower=1> n_intervals;  // intervals during which sites are visited
-  int<lower=1> interval[R]; // vector of intervals (used as covariate data for 
+  
+  real intervals[n_intervals]; // vector of intervals (used as covariate data for 
                                 // fixed effect of occupancy interval (time) on occupancy)
   
   int<lower=1> n_visits; // visits within intervals
   
-  int<lower=0> V[R, n_visits];  // visits when species i was detected at site j on interval k
+  int<lower=0> V[n_species, n_sites, n_intervals, n_visits];  // visits when species i was detected at site j on interval k
+  
   
 } // end data
 
@@ -87,34 +59,43 @@ parameters {
 
 transformed parameters {
   
-  real logit_psi[n_species*n_sites*n_intervals];  // log odds  of occurrence
-  real logit_p[R, n_visits];  // log odds of detection
+  real psi[n_species, n_sites, n_intervals];  // log odds  of occurrence
+  real p[n_species, n_sites, n_intervals, n_visits];  // log odds of detection
   
-  for (i in 1:R){   // loop across all species*site*intervals
+  for (i in 1:n_species){   // loop across all species
+    for (j in 1:n_sites){    // loop across all sites
+      for(k in 1:n_intervals){
           
-          logit_psi[i] = // log odds  of occurrence is equal to
+          psi[i, j, k] = inv_logit( // log odds  of occurrence is equal to
             mu_psi_0 + // a baseline intercept
             psi_species[species[i]] + // a species specific intercept
-            psi_interval[species[i]]*interval[i]; // a species specific temporal effect
-            
+            psi_interval[species[i]]*intervals[k] // a species specific temporal effect
+            );
+      }
+    }
   }
   
-  for (i in 1:R){   // loop across all species*site*intervals
-    for (j in 1:n_visits){
-          
-          logit_p[i, j] = // log odds  of detection is equal to
+  for (i in 1:n_species){   // loop across all species
+    for (j in 1:n_sites){    // loop across all sites
+      for(k in 1:n_intervals){
+        for(l in 1:n_visits){
+        
+          p[i, j, k, l] = inv_logit( // log odds of detection is equal to
             mu_p_0 + // a baseline intercept
             p_species[species[i]] + // a species specific intercept
-            p_site[site[i]] + // a spatially specific intercept
-            p_interval*interval[i]; // an overall effect of time on detection
-            
-    }       
+            p_site[sites[j]] + // a spatially specific intercept
+            p_interval*intervals[k] // an overall effect of time on detection
+           );
+        }
+      }
+    }
   }
   
 } // end transformed parameters
 
 
 model {
+  
   // PRIORS
   
   // Occupancy (Ecological Process)
@@ -125,11 +106,12 @@ model {
   // distribution (variance defined by sigma), centered at 0. 
   sigma_psi_species ~ cauchy(0, 2.5);
   
-  psi_interval ~ normal(0, sigma_psi_interval);
+  psi_interval ~ normal(mu_psi_interval, sigma_psi_interval);
   // occupancy slope (temporal effect on occupancy) for each species drawn from the 
   // community distribution (variance defined by sigma), centered at mu_psi_interval. 
   // centering on mu (rather than 0) allows us to estimate the average effect of
   // the management on abundance across all species.
+  mu_psi_interval ~ cauchy(0, 2.5); // community mean
   sigma_psi_interval ~ cauchy(0, 2.5); // community variance
   
   // Detection (Observation Process)
@@ -140,7 +122,6 @@ model {
   // distribution (variance defined by sigma), centered at 0. 
   sigma_p_species ~ cauchy(0, 2.5);
   
-  // multivariate hierarchical prior
   p_site ~ normal(0, sigma_p_site);
   // detection intercept for each site*interval drawn from the spatiotemporal
   // distribution (variance defined by sigma), centered at 0. 
@@ -149,27 +130,43 @@ model {
   p_interval ~ cauchy(0, 2.5);
   
   // LIKELIHOOD
-  // Stan can sample mean and sd of parameters by summing out the
+  
+  // Stan can sample the mean and sd of parameters by summing out the
   // parameter (marginalizing) across likelihood statements
-  for(i in 1:R) { // loop across all interval*site*species combinations
-    for(j in 1:n_visits) { // loop across all visits
+  for(i in 1:n_species) { // loop across all species
+    for(j in 1:n_sites) { // loop across all sites
+      for(k in 1:n_intervals){
+        for(l in 1:n_visits){
           
           // if species is detected at the specific site*interval at least once
           // lp_observed calculates the probability density that occurs given logit_psi plus
           // the probability density that we did/did not observe it on each visit l in 1:nvisit
-          if(sum(V[i,1:n_visits]) > 0){ 
-            target += lp_observed(V[i, j], 
-              logit_psi[i], logit_p[i, j]);
+          if(sum(V[i, j, k,1:n_visits]) > 0) {
+            
+             // should wrap this in function lp_observed:
+             target += log(psi[i,j,k]);
+             target += bernoulli_lpmf(V[i,j,k] | p[i,j,k]); //vectorized over repeat sampling events l
           
           // else the species was never detected at the site*interval
           // lp_unobserved sums the probability density of:
           // 1) species occupies the site*interval but was not detected on each visit, and
           // 2) the species does not occupy the site*interval
           } else {
-            target += lp_unobserved(logit_psi[i], logit_p[i, j]);
+            
+            // should wrap this in a function lp_unobserved
+            // currently written as log1m(p) for each visit in 1:n_visits (manually defined below as 1, 2, 3...)
+            // should be proper to wrap this up in a single statement that increments the log probability
+            // for each p in 1:n_visit, but simply putting log1m(p[i,j,k,l]) doesn't seem to work 
+            // for reasons that I don't understand. This way below works, it's just a bit too brute force
+            // and needs to be rewritten to accomodate the specific number of max visits
+            target += log_sum_exp(log(psi[i,j,k]) + 
+                                 log1m(p[i,j,k,1]) + log1m(p[i,j,k,2]) + log1m(p[i,j,k,3]), 
+                          log1m(psi[i,j,k]));
             
           } // end if/else
           
+        }
+      }
     }
   }
 } // end model
---
+++ simulation/simulate_simplest.R
@@ -0,0 +1,251 @@
+## Data simulation for simplest form of the data
+# single season, species considered across all sites (not just range)
+# and across all visits (not considering that sites without any detections 
+# for any species were probably not visited or that sites with only detections for
+# a few species only might be targeted and not fully community wide)
+
+## --------------------------------------------------
+### Variable values for data simulation
+## study dimensions
+n_species = 20 ## number of species
+n_sites = 15 ## number of sites
+n_intervals = 8 ## number of occupancy intervals
+n_visits = 3 ## number of samples per year
+
+## occupancy
+mu_psi_0 = -0.5
+sigma_psi_species = 0.5
+mu_psi_interval = 0.5
+sigma_psi_interval = 0.2
+
+## detection
+mu_p_0 = -0.5
+p_interval = 0.25 # detection probability increasing with time
+sigma_p_site = 0.3
+sigma_p_species = 0.3
+
+## visit
+# will need to come back to this portion once simple part is worked out
+# mu.v.0 = 0 
+# mu.v.interval = -0.5
+
+## type sym
+# will need to come back to this portion once simple part is worked out
+# type.range = "equal"
+# type.visit = 'visit_miss'
+# prop.visits.same = 1
+
+## expit and logit functions
+expit <- function(x) 1/(1+exp(-x))
+illogit <- function(x) exp(x)/(1+exp(x))
+logit <- function(x) log(x/(1-x))
+
+# Interval values: numeric vector (will act as covariate data for psi.interval)
+intervals <- seq(1, n_intervals, by=1)
+intervals <- intervals - 1
+sites <- seq(1, n_sites, by=1)
+species <- seq(1, n_species, by=1)
+
+## --------------------------------------------------
+### specify species-specific occupancy probabilities
+
+## species-specific random intercepts
+psi_species <- rnorm(n=n_species, mean=0, sd=sigma_psi_species)
+# species baseline occupancy is drawn from a normal distribution with mean 0 and 
+# species specific variation defined by sigma.psi.sp
+
+## effect of interval on occupancy (species-specific random slopes)
+psi_interval <- rnorm(n=n_species, mean=mu_psi_interval, sd=sigma_psi_interval)
+# change in each species occupancy across time is drawn from a distribution defined
+# by a community mean (mu_psi_interval) with 
+# species specific variation defined by sigma_psi_interval
+
+## --------------------------------------------------
+### specify species-specific detection probabilities
+
+## species-specific random intercepts
+p_species  <- rnorm(n=n_species, mean = 0, sd=sigma_p_species)
+# species baseline detection probability is drawn from a normal distribution with mean 0 and 
+# species specific variation defined by sigma.p.sp
+
+## effect of site and interval on detection (site,interval-specific random slopes)
+p_site <- rnorm(n=n_sites, mean=0, sd=sigma_p_site)
+
+# spatiotemporal variability in detection probability (changing across sites and 
+# occupancy intervals), and helps account for the variation that is inherent in 
+# sample effort across space and time in unstructured historical datasets. 
+# spatiotemporal variability at each site*interval is drawn from a distribution defined
+# by a mean of 0 and with site*interval specific variation of sigma.p.site
+
+## --------------------------------------------------
+## Create arrays for psi and p
+psi_matrix <- array(NA, dim =c(n_species, n_sites, n_intervals)) 
+# a psi value for each species, at each site, in each interval 
+
+p_matrix <- array(NA, dim =c(n_species, n_sites, n_intervals, n_visits))
+# a p value for each species, at each site, in each interval, AND in each visit
+
+for(species in 1:n_species) { # for each site
+  for(site in 1:n_sites) { # for each interval
+    for(interval in 1:n_intervals) { # for each species
+      
+      psi_matrix[species, site, interval] <- illogit( # occupancy is equal to
+        mu_psi_0 + # a baseline intercept
+          psi_species[species] + # a species specific intercept
+          psi_interval[species]*intervals[interval] # a species specific temporal change
+      )
+          
+      for(visit in 1:n_visits) { # for each visit
+        
+        p_matrix[species, site, interval, visit] <- illogit( # detection is equal to 
+          mu_p_0 + # a baseline intercept
+            p_species[species] + # a species specific intercept
+            p_site[site] + # a spatiotemporally specific intercept
+            p_interval*intervals[interval] # an overall effect of time on detection
+        )
+          
+      } # for each visit
+    } # for each species
+  } # for each interval
+} # for each site
+
+# preview the psi and p arrays
+head(psi_matrix[1:n_species, 1:n_sites,1])
+head(p_matrix[1:n_species, 1:n_sites,7,1])
+
+# (p_matrix[1,1,1:n_intervals,1]) # if p.interval is >0 these should generally be increasing from low to high
+# (p_matrix[2,1,1:n_intervals,1]) # if p.interval is >0 these should generally be increasing from low to high
+# (p_matrix[1,2,1:n_intervals,1]) # if p.interval is >0 these should generally be increasing from low to high
+
+## --------------------------------------------------
+## Generate presence and absence
+
+Z <- array(NA, dim=c(n_species=n_species,
+                     n_sites=n_sites,
+                     n_intervals=n_intervals))
+
+for(interval in 1:n_intervals){
+  for(site in 1:n_sites){
+    for(species in 1:n_species){
+      
+      # eventually here we will need to specify which(site) to restrict to actual range
+      Z[species,site,interval] <- rbinom(n = 1, size = 1, 
+                                         prob = psi_matrix[species,site,interval])
+      
+    }
+  }
+}
+
+## --------------------------------------------------
+## Generate detection non detection data 
+
+V <- array(NA, dim=c(n_species=n_species,
+                     n_sites=n_sites,
+                     n_intervals=n_intervals,
+                     n_visits=n_visits))
+
+for(interval in 1:n_intervals){
+  for(site in 1:n_sites){
+    for(species in 1:n_species){
+      for(visit in 1:n_visits){
+        
+        # eventually here we will need to specify which(site) to restrict to actual range
+        V[species,site,interval,visit] <- Z[species,site,interval] * # occupancy state * detection prob
+          rbinom(n = 1, size = 1, prob = p_matrix[species,site,interval,visit])
+        
+      }
+    }
+  }
+}
+
+# View(as.data.frame(psi_matrix[1:n_species,1:n_sites,1]))
+# View(as.data.frame(Z[1:n_species,1:n_sites,1]))
+
+# View(as.data.frame(p_matrix[1:n_species,1:n_sites,1,1]))
+# View(as.data.frame(V[1:n_species,1:n_sites,1,1]))
+
+
+## --------------------------------------------------
+### Prepare data for model
+
+# data to feed to the model
+V <- V # detection data
+n_species <- n_species # number of species
+n_sites <- n_sites # number of sites
+n_intervals <- n_intervals # number of surveys 
+n_visits <- n_visits
+
+intervals_raw <- seq(1, n_intervals, by=1)
+intervals <- intervals_raw - 1
+sites <- seq(1, n_sites, by=1)
+species <- seq(1, n_species, by=1)
+
+stan_data <- c("V", 
+               "n_species", "n_sites", "n_intervals", "n_visits", 
+               "intervals", "species", "sites") 
+
+# Parameters monitored
+params <- c("mu_psi_0",
+            #"psi_species",
+            "sigma_psi_species",
+            # "psi_interval",
+            "mu_psi_interval",
+            "sigma_psi_interval",
+            "mu_p_0",
+            # "p_species",
+            "sigma_p_species",
+            # "p_site",
+            "sigma_p_site",
+            "p_interval"
+)
+
+
+# MCMC settings
+n_iterations <- 1200
+n_thin <- 1
+n_burnin <- 600
+n_chains <- 4
+n_cores <- n_chains
+
+## Initial values
+# given the number of parameters, the chains need some decent initial values
+# otherwise sometimes they have a hard time starting to sample
+inits <- lapply(1:n_chains, function(i)
+  
+  list(mu_psi_0 = runif(1, -1, 1),
+       sigma_psi_species = runif(1, 0, 1),
+       mu_psi_interval = runif(1, -1, 1),
+       sigma_psi_interval = runif(1, 0, 1),
+       mu_p_0 = runif(1, -1, 1),
+       sigma_p_species = runif(1, 0, 1),
+       sigma_p_site = runif(1, 0, 1),
+       p_interval = runif(1, -1, 1)
+       
+  )
+)
+
+#targets <- as.data.frame(cbind(param_names, parameters))
+
+## --------------------------------------------------
+### Run model
+library(rstan)
+stan_model <- "./simulation/model_simplest.stan"
+
+## Call Stan from R
+stan_out_sim <- stan(stan_model,
+                     data = stan_data, 
+                     init = inits, 
+                     pars = params,
+                     chains = n_chains, iter = n_iterations, 
+                     warmup = n_burnin, thin = n_thin,
+                     seed = 1,
+                     open_progress = FALSE,
+                     cores = n_cores)
+
+print(stan_out_sim, digits = 3)
+saveRDS(stan_out_sim, "./simulation/stan_out_sim.rds")
+
+library(shinystan)
+shinystan::launch_shinystan(stan_out_sim)
+
+psi_species
