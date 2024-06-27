27880a1e5dc27fd5b25115e151ea50a8be1a5196
jensculrich <jensj27@gmail.com>
2024-06-27 13:12:58 -0700

    regathered data for all sensitivity analyses

--- occupancy/analysis/run_model.R
+++ occupancy/analysis/run_model.R
@@ -19,8 +19,8 @@ n_visits = 3 # must define the number of repeat obs years within each interval
 min_records_per_species = 5 # filters species with less than this many records (total between both datasets)..
 min_unique_detections = 2 # species must have more detections at more site/years than this (so 2 == 3 or more unique detections)
 # within the time span defined above
-grid_size = 10000 # in metres so, e.g., 25000 = 25km x 25 km 
-min_population_size = 1200 # min pop density in the grid cell (per km^2)
+grid_size = 25000 # in metres so, e.g., 25000 = 25km x 25 km 
+min_population_size = 600 # min pop density in the grid cell (per km^2)
 
 min_species_for_community_sampling_event = 2 # community sampling inferred if..
 # species depositied in single institution from a site in a single year is >= min_species_for_community_sampling_event
@@ -61,8 +61,8 @@ n_visits = 3 # must define the number of repeat obs years within each interval
 min_records_per_species = 5 # filters species with less than this many records (total between both datasets)..
 min_unique_detections = 1 # filters species not detected at unique sites in unique years at/below this value
 # within the time span defined above (is only from urban sites, should redefine to be from anywhere)
-grid_size = 10000 # in metres so, e.g., 25000 = 25km x 25 km 
-min_population_size = 1200 # min pop density in the grid cell (per km^2)
+grid_size = 25000 # in metres so, e.g., 25000 = 25km x 25 km 
+min_population_size = 600 # min pop density in the grid cell (per km^2)
 
 min_species_for_community_sampling_event = 2 # community sampling inferred if..
 # species depositied in single institution from a site in a single year is >= min_species_for_community_sampling_event
@@ -373,9 +373,9 @@ if(taxon == "bombus"){
     )
     
     # MCMC settings
-    n_iterations <- 4000
+    n_iterations <- 300
     n_thin <- 1
-    n_burnin <- 2000
+    n_burnin <- 150
     n_chains <- 4
     #n_cores <- parallel::detectCores()
     n_cores <- 4
